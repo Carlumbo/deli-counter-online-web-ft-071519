b8385efeea5bff0ea94af18921d09c43e2578d25
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-16 19:51:02 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,12 +1,12 @@
 katz_deli = []
-other_deli = 
+other_deli = ["Logan", "Avi", "Spencer"]
 def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   end
 end 
 
-def take_a_number(deli,name)
+def take_a_number(deli)
   count = 0 
   phrase = []
   while count < array.length do
@@ -15,3 +15,5 @@ def take_a_number(deli,name)
   end 
   puts phrase
 end 
+
+take_a_number(other_deli)
