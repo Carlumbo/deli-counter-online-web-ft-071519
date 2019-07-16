b3a7f61346608dec9f857e29532a2f9266916ae6
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-16 19:43:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ end
 
 def take_a_number(other_deli)
   other_deli.each_with_index.map do 
-    |name, number|
-    "The line is currently: #{number +1}. #{name}"
+    |name, index|
+    "The line is currently: #{index+1}. #{name}"
   end 
 end 
