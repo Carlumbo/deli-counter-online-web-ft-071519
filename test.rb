4b31305ee09b6141b23fa6dd8426ab23ed837ff1
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-16 19:50:04 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,17 @@
+katz_deli = []
+other_deli
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  end
+end 
+
+def take_a_number(deli,name)
+  count = 0 
+  phrase = []
+  while count < array.length do
+  phrase << "The line is currently:#{count +1}. #{deli}"
+  count += 1 
+  end 
+  puts phrase
+end 
