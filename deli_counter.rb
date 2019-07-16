21d109de00597cbeb4c31a587bc169bf6297967c
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-16 19:40:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,10 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
-  end 
+  end
 end 
+
+def take_a_number(array)
+  array.each_with_index.map do 
+    |name, number|
+    puts "The line is currently: #{number +1}. "
